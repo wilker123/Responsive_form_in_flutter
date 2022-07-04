bef125ec4452580ef6960950c5e91a027c79235d
wilker123 <josewilkerlucena123@gmail.com>
2022-07-04 16:40:22 -0300

    Finalizing responsive form

--- responsive_form/lib/pages/home_page.dart
+++ responsive_form/lib/pages/home_page.dart
@@ -35,89 +35,138 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 52,
-              ),
-              Text(
-                "Sign Up to Masterminds",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w700,
-                  color: darkTextColor,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 52),
+                Text(
+                  "Sign Up to Masterminds",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: darkTextColor,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Wrap(
-                children: [
-                  Text(
-                    "Already have an account? ",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: lightTextColor,
-                    ),
-                  ),
-                  Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: lightTextColor,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 24),
-              getTextField(hint: "Full Name"),
-              const SizedBox(height: 16),
-              getTextField(hint: "Email"),
-              const SizedBox(height: 16),
-              getTextField(hint: "Password"),
-              const SizedBox(height: 16),
-              getTextField(hint: "Confirm Password"),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(purpleColor),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.symmetric(vertical: 14)),
-                    textStyle: MaterialStateProperty.all(
-                      const TextStyle(
+                const SizedBox(height: 4),
+                Wrap(
+                  children: [
+                    Text(
+                      "Already have an account? ",
+                      style: TextStyle(
                         fontSize: 14,
-                        fontWeight: FontWeight.w700,
+                        fontWeight: FontWeight.w600,
+                        color: lightTextColor,
                       ),
                     ),
-                  ),
-                  child: Text("Create Account"),
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: lightTextColor,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  const Expanded(child: Divider()),
-                  const SizedBox(width: 16),
-                  Text(
-                    "or sign up via",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      color: lightTextColor,
+                const SizedBox(height: 24),
+                getTextField(hint: "Full Name"),
+                const SizedBox(height: 16),
+                getTextField(hint: "Email"),
+                const SizedBox(height: 16),
+                getTextField(hint: "Password"),
+                const SizedBox(height: 16),
+                getTextField(hint: "Confirm Password"),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(purpleColor),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.symmetric(vertical: 14)),
+                      textStyle: MaterialStateProperty.all(
+                        const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    child: Text("Create Account"),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    const Expanded(child: Divider()),
+                    const SizedBox(width: 16),
+                    Text(
+                      "or sign up via",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: lightTextColor,
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    const Expanded(child: Divider()),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all(BorderSide(
+                        color: borderColor,
+                      )),
+                      foregroundColor: MaterialStateProperty.all(darkTextColor),
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.symmetric(vertical: 14)),
+                      textStyle: MaterialStateProperty.all(
+                        const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        SizedBox(width: 10),
+                        Text("Google"),
+                      ],
                     ),
                   ),
-                  const SizedBox(width: 16),
-                  const Expanded(child: Divider()),
-                ],
-              )
-            ],
+                ),
+                const SizedBox(height: 16),
+                Wrap(
+                  children: [
+                    Text(
+                      "By signing up to Masterminds you agree to our",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: lightTextColor,
+                      ),
+                    ),
+                    Text(
+                      "Terms and conditions",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: lightTextColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
