47b31ccafbef3179a4410969604e7ec7f58c1d64
wilker123 <josewilkerlucena123@gmail.com>
2022-07-04 15:09:58 -0300

    Defining the project colors and creating the initial structure

--- responsive_form/lib/main.dart
+++ responsive_form/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'pages/home_page.dart';
 
@@ -9,15 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
       ),
-      home: const HomePage(),
+      designSize: Size(360, 640),
     );
   }
 }
--- responsive_form/lib/pages/home_page.dart
+++ responsive_form/lib/pages/home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final purpleColor = Color(0xff6688FF);
+  final darkTextColor = Color(0xff1F1A3D);
+  final lightTextColor = Color(0xff999999);
+  final textFieldColor = Color(0xffF5F6FA);
+  final borderColor = Color(0xffD9D9D9);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Sign Up to Masterminds"),
+          ],
+        ),
+      ),
+    );
   }
 }
