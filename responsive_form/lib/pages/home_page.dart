76ad10e2cff3cc2af5fa49b884f74005ab08a32d
wilker123 <josewilkerlucena123@gmail.com>
2022-07-04 15:49:30 -0300

    Created the text fields and the create an account button

--- responsive_form/lib/pages/home_page.dart
+++ responsive_form/lib/pages/home_page.dart
@@ -1,22 +1,124 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final purpleColor = Color(0xff6688FF);
-  final darkTextColor = Color(0xff1F1A3D);
-  final lightTextColor = Color(0xff999999);
-  final textFieldColor = Color(0xffF5F6FA);
-  final borderColor = Color(0xffD9D9D9);
+  final purpleColor = const Color(0xff6688FF);
+  final darkTextColor = const Color(0xff1F1A3D);
+  final lightTextColor = const Color(0xff999999);
+  final textFieldColor = const Color(0xffF5F6FA);
+  final borderColor = const Color(0xffD9D9D9);
+
+  Widget getTextField({required String hint}) {
+    return TextField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.transparent, width: 0),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.transparent, width: 0),
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+          filled: true,
+          fillColor: textFieldColor,
+          hintText: "First Name",
+          hintStyle: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          )),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Sign Up to Masterminds"),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 52,
+              ),
+              Text(
+                "Sign Up to Masterminds",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: darkTextColor,
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Wrap(
+                children: [
+                  Text(
+                    "Already have an account? ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: lightTextColor,
+                    ),
+                  ),
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: lightTextColor,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 24),
+              getTextField(hint: "Full Name"),
+              const SizedBox(height: 16),
+              getTextField(hint: "Email"),
+              const SizedBox(height: 16),
+              getTextField(hint: "Password"),
+              const SizedBox(height: 16),
+              getTextField(hint: "Confirm Password"),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(purpleColor),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(vertical: 14)),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  child: Text("Create Account"),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  const Expanded(child: Divider()),
+                  const SizedBox(width: 16),
+                  Text(
+                    "or sign up via",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: lightTextColor,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  const Expanded(child: Divider()),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
